f4ce63e0f6aa6b67419db18cb30f53eebc105972
Juan Carlos Llorente Bedoya <124886197+JuanllorenteB@users.noreply.github.com>
2023-10-12 20:25:07 -0500

    commit 3
    creacion del proyecto

--- Prestamos_deportivos/Prestamos_deportivos/Program.cs
+++ Prestamos_deportivos/Prestamos_deportivos/Program.cs
@@ -16,7 +16,7 @@ namespace Prestamos_deportivos
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new frm_ModuloUsuarios());
         }
     }
 }
